acece6ec72c865637d28a6b88c1876d02978e173
rinkorn <rinkorn@mail.ru>
2017-09-05 15:13:12 +0300

    add settings

---
+++ init.sh
@@ -0,0 +1,9 @@
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+
+
+# git clone https://github.com/your_account/stepic_web_project.git /home/box/web
+# bash /home/box/web/init.sh
