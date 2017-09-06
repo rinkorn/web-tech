8d7b7b776fd95fb3fef13d9f42dc1bd39a416898
rinkorn <rinkorn@mail.ru>
2017-09-06 10:53:13 +0300

    work1

--- init.sh
+++ init.sh
@@ -1,9 +1,4 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
-
+# ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# /etc/init.d/nginx restart
 
 # git clone https://github.com/your_account/stepic_web_project.git /home/box/web
-# bash /home/box/web/init.sh
