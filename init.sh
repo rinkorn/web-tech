9b50449ccb6e86c60c8e16e56dbc353878f26240
rinkorn <rinkorn@mail.ru>
2017-09-07 13:30:57 +0300

    Task 1.9. Add to init.py gunicorn link

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 # ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-# /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
+# sudo rm /etc/nginx/sites-enabled/default
+# sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+
+sudo /etc/init.d/nginx restart
 
 # https://github.com/search?utf8=✓&q=stepic_web_project
